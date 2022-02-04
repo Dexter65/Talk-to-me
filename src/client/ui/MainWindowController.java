b9084d59530ab925c186a790bfbac60d223edf43
unknown <qw65russ@gmail.com>
2022-02-04 23:36:26 +0300

    Added client's ui base and FXcontroller class (MainWindowController)

---
+++ src/client/ui/MainWindowController.java
@@ -0,0 +1,15 @@
+package client.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+
+    @FXML
+    private TextField userInput;
+    @FXML
+    private TextArea chatOutput;
+
+    
+}
